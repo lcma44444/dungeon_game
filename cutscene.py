0b7655adf567f32ef0bd63c7bfe97c5146620140
lcma44444 <malongching@gmail.com>
2026-06-16 17:45:20 +0800

    add cutscene

---
+++ cutscene.py
@@ -0,0 +1,17 @@
+import cv2
+def show_cutscene():
+    img = cv2.imread("images/title.png")
+    img[-100:] = 0  # last 100 pixel rows are black
+    img = cv2.putText(
+    img,
+    "Hello World",
+    org=(15, 490),  # x/y position of the text
+    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+    fontScale=1,
+    color=(255, 255, 255),  # white
+    thickness=2,
+)
+    cv2.imshow("Cutscene", img)
+    cv2.waitKey(3)
+    cv2.destroyAllWindows()
+    
